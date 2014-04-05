439c3c31ca7b803916508629d9f40a2c47a908f7
JOLLY <evifere@gmail.com>
2014-04-05 16:12:27 +0200

    add vendor

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+
+var paths = {
+  scripts: ['src/js/vendor/*.js', 'src/js/core/*.js']
+};
+
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('scripts', function() {
+  // Minify and copy all JavaScript (except vendor scripts)
+  return gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('build/js'));
 });
