768627a8354306f685eb0ce0228fdaee956b0d64
JOLLY <evifere@gmail.com>
2014-04-06 10:08:44 +0200

    indent

--- src/js/core/edWuiBootStrap.js
+++ src/js/core/edWuiBootStrap.js
@@ -1,13 +1,8 @@
 (function(win, doc, edWui){
 
     win.tpl = function(view) {
-        console.log(view + '-viewtpl');
-        console.log(doc.getElementById(view + '-viewtpl'));
-        
       return _.template(doc.getElementById(view + '-viewtpl').innerHTML);
     };
-
-
 
     edWui.Models = {};
     edWui.Models.Instances = {};
@@ -18,9 +13,5 @@
     edWui.AppRouter = {};
     edWui.AppRouter.Instance = {};
     edWui.Events = {};
-
-
-
-
 
 })(window, window.document, window.edWui || (window.edWui = {}));
